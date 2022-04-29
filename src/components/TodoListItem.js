0620061e4979f86dfecbe1a1869694c469930061
DivineChuks <chuksbon6@gmail.com>
2022-04-29 13:35:19 +0100

    more commit

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Col } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { setCompleted } from "../redux/todoSlice";
